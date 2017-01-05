7868147f1eb3f79cc5717630fcaefeef02ae3148
Boothand <boothand@hotmail.com>
2017-01-05 16:08:59 +0100

    Rough and a little buggy energy flow management system

--- Assets/Scripts/Engineer.cs
+++ Assets/Scripts/Engineer.cs
@@ -3,21 +3,97 @@ using System.Collections;
 
 public class Engineer : MechComponent
 {
-	public float legsEnergy { get; private set; }
-	public float armsEnergy { get; private set; }
-	public float engineerEnergy { get; private set; }
+	public enum EnergyComponents
+	{
+		Helm,
+		Weapons,
+		Engineer
+	}
+
+	public int helmIndex = (int)EnergyComponents.Helm;
+	public int weaponsIndex = (int)EnergyComponents.Weapons;
+	public int engineerIndex = (int)EnergyComponents.Engineer;
+
+
+	public float[] energies;
 
 	protected override void OnAwake()
 	{
 		base.OnAwake();
+		energies = new float[3];
 
-		legsEnergy = 0.5f;
-		armsEnergy = 1f;
-		engineerEnergy = 1f;
+		energies[helmIndex] = 0.75f;
+		energies[weaponsIndex] = 0.125f;
+		energies[engineerIndex] = 0.125f;
 	}
-	
-	void Update ()
+
+	//0.5  +	0.02 =		0.52
+	//0.5 - 	0.01 =		0.49
+	//0 -		0.01 =		-0.01
+
+	//0 -		0.02 =		-0.02
+	//0.5  +	0.01 =		0.51
+	//0.5 + 	0.01 =		0.51
+
+	public void AddEnergy(EnergyComponents component, float amount)
 	{
-		
+		int index = (int)component;
+
+		for (int i = 0; i < energies.Length; i++)
+		{
+			if (i == index) //The component to add to
+			{
+				energies[i] += amount;
+
+				if (energies[i] < 0f)
+				{
+					for (int j = 0; j < energies.Length; j++)
+					{
+						if (j == index)
+							continue;
+
+						energies[j] += energies[index] / 2;
+						//energies[index] += energies[index] / 2;
+					}
+				}
+			}
+			else //All other components
+			{
+				energies[i] -= amount / 2;
+
+				//If there wasn't enough energy to subtract from
+				if (energies[i] < 0f)
+				{
+					int belowIndex = i;
+
+					//Add the part below 0 to the remaining component, subtract the double amount from the target
+					for (int j = 0; j < energies.Length; j++)
+					{
+						if (j == belowIndex || j == index)
+							continue;
+
+						energies[j] -= energies[belowIndex];
+						energies[index] += energies[belowIndex] * 2;
+					}
+				}
+			}
+
+			energies[i] = Mathf.Clamp01(energies[i]);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			AddEnergy(EnergyComponents.Helm, -0.02f);
+		}
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			AddEnergy(EnergyComponents.Helm, 0.02f);
+		}
+
+		float sum = energies[0] + energies[1] + energies[2];
+		//print(sum);
 	}
 }
---
+++ Assets/Scripts/UI/EnergySliders.cs
@@ -0,0 +1,36 @@
+﻿//using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergySliders : MechComponent
+{
+	[SerializeField]
+	Slider helmSlider, armsSlider, engineerSlider;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	public void SetEnergy(int index)
+	{
+		Slider sliderToUse = helmSlider;
+
+		if (index == 1)
+			sliderToUse = armsSlider;
+		if (index == 2)
+			sliderToUse = engineerSlider;
+
+		Engineer.EnergyComponents energyIndex = (Engineer.EnergyComponents)index;
+		float amount = sliderToUse.value - engineer.energies[index];
+		print(energyIndex + " " + amount);
+		engineer.AddEnergy(energyIndex, amount);
+	}
+
+	void Update()
+	{
+		helmSlider.value = engineer.energies[0];
+		armsSlider.value = engineer.energies[1];
+		engineerSlider.value = engineer.energies[2];
+	}
+}
