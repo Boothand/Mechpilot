6b2c2965498a15b80040bc2b932dd3d6d2632b3e
Boothand <boothand@hotmail.com>
2017-01-09 10:04:54 +0100

    Percentages added below energy sliders

--- Assets/Scripts/UI/EnergySliders.cs
+++ Assets/Scripts/UI/EnergySliders.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 
 public class EnergySliders : MechComponent
 {
-	[SerializeField]
-	Slider[] sliders = new Slider[3];
+	[SerializeField] Slider[] sliders = new Slider[3];
+	[SerializeField] Text[] sliderValueTexts = new Text[3];
 	[SerializeField] Transform energyIndicator;
 
 	protected override void OnAwake()
@@ -25,10 +25,14 @@ public class EnergySliders : MechComponent
 		for (int i = 0; i < sliders.Length; i++)
 		{
 			sliders[i].value = engineer.energies[i];
+			sliderValueTexts[i].text = (engineer.energies[i]).ToString("00.00%");
 		}
 
+		//Set the scale of the horizontal energy meter bar
+		float lerpTime = 5f;
 		Vector3 uiScale = energyIndicator.localScale;
-		uiScale.x = Mathf.Lerp(uiScale.x, engineer.energySum, Time.deltaTime * 5f);
+
+		uiScale.x = Mathf.Lerp(uiScale.x, engineer.energySum, Time.deltaTime * lerpTime);
 		energyIndicator.localScale = uiScale;
 	}
 }
