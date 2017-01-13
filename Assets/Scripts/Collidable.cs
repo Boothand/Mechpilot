5887c7593a7dd6ca94b3ee5f052166e331ab3d58
Boothand <boothand@hotmail.com>
2017-01-13 16:08:28 +0100

    A way to detect and get collision events, such as sword collisions

---
+++ Assets/Scripts/Collidable.cs
@@ -0,0 +1,46 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class Collidable : MechComponent
+{
+	public delegate void Collide();
+	public event Collide OnCollision;
+
+	[SerializeField] LayerMask layerMask;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	bool IsInLayerMask(GameObject obj, LayerMask mask)
+	{
+		int bit = (1 << obj.layer);
+
+		if ((mask.value & bit) > 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.transform.root != transform.root &&
+			IsInLayerMask(col.gameObject, layerMask))
+		{
+
+			print("Collided with " + col.name);
+			if (OnCollision != null)
+			{
+				OnCollision();
+			}
+		}
+	}
+
+	void Update()
+	{
+		
+	}
+}
