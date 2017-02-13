f572c0a236ce5c60af596b49dfcf1ff2a31c394e
Boothand <boothand@hotmail.com>
2017-02-13 17:13:25 +0100

    Re-organized AI to make room for defending tactic

---
+++ Assets/Scripts/AI_Defender.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class AI_Defender : AI_MechComponent
+{
+
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+
+		if (aiCombat.combatState == AI_Combat.CombatState.Defend)
+		{
+
+		}
+	}
+}
