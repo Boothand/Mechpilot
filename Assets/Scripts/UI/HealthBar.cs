13358d0ee1da13165e26c9464b8ea3c7f5a3052e
Boothand <boothand@hotmail.com>
2017-02-13 13:57:49 +0100

    Health bar displayed over head

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class HealthBar : MechComponent
+{
+	[SerializeField] Transform healthBar;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	void Update()
+	{
+		Vector3 healthScale = healthBar.transform.localScale;
+
+		float targetScale = (float)healthManager.getHealth / (float)healthManager.getMaxHealth;
+		healthScale.y = Mathf.Lerp(healthScale.y, targetScale, Time.deltaTime * 4f);
+
+		healthBar.localScale = healthScale;
+	}
+}
