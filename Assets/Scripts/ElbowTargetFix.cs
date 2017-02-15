4d3052265304d11286d38cb256e8dfacbb68392e
Boothand <boothand@hotmail.com>
2017-02-15 12:04:42 +0100

    Elbow targets in a logical place!

---
+++ Assets/Scripts/ElbowTargetFix.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class ElbowTargetFix : MechComponent
+{
+	[SerializeField] Transform elbowL, elbowR;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	void Update()
+	{
+		elbowR.position = hierarchy.rhand.position - hierarchy.rhand.forward * 100f;
+		elbowL.position = hierarchy.lHand.position - hierarchy.lHand.forward * 100f;
+
+		elbowR.position = new Vector3(elbowR.position.x, hierarchy.rhand.position.y - 100f, elbowR.position.z);
+		elbowL.position = new Vector3(elbowL.position.x, hierarchy.lHand.position.y - 100f, elbowL.position.z);
+		Debug.DrawLine(hierarchy.rhand.position, elbowR.position, Color.red);
+	}
+}
