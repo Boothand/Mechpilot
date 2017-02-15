556fdd848e9e0ddce590f7227ac0eb9d1606129c
Boothand <boothand@hotmail.com>
2017-02-15 13:58:37 +0100

    Adjusted left elbow target, more left-oriented

--- Assets/Scripts/ElbowTargetFix.cs
+++ Assets/Scripts/ElbowTargetFix.cs
@@ -13,10 +13,12 @@ public class ElbowTargetFix : MechComponent
 	void Update()
 	{
 		elbowR.position = hierarchy.rhand.position - hierarchy.rhand.forward * 100f;
-		elbowL.position = hierarchy.lHand.position - hierarchy.lHand.forward * 100f;
+		//elbowL.position = hierarchy.lHand.position - hierarchy.lHand.forward * 100f;
+		elbowL.position = mech.transform.position - mech.transform.right * 35f + Vector3.down * 100f;
+
 
 		elbowR.position = new Vector3(elbowR.position.x, hierarchy.rhand.position.y - 100f, elbowR.position.z);
-		elbowL.position = new Vector3(elbowL.position.x, hierarchy.lHand.position.y - 100f, elbowL.position.z);
-		Debug.DrawLine(hierarchy.rhand.position, elbowR.position, Color.red);
+		//elbowL.position = new Vector3(elbowL.position.x, hierarchy.lHand.position.y - 100f, elbowL.position.z);
+		Debug.DrawLine(hierarchy.lHand.position, elbowL.position, Color.red);
 	}
 }
