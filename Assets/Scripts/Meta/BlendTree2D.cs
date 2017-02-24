9c6aceab992bfa3a9242da64fdb0e86089bf4d75
Boothand <boothand@hotmail.com>
2017-02-24 15:06:58 +0100

    In the middle of trying out a new way of setting rotations

---
+++ Assets/Scripts/Meta/BlendTree2D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class BlendTree2D
+{
+	struct BlendPoint
+	{
+		public Vector3 pos;
+		public float x, y;
+
+		public BlendPoint(Vector3 pos, float x, float y)
+		{
+			this.pos = pos;
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static List<BlendPoint> pointList = new List<BlendPoint>();
+
+	public static void Add(Vector3 pos, float x, float y)
+	{
+		BlendPoint point = new BlendPoint(pos, x, y);
+
+		pointList.Add(point);
+	}
+
+	//public static Vector3 Get(float x, float y)
+	//{
+	//	bool top = true;
+	//	bool right = true;
+	//}
+
+	public static Vector3 BlendedPos(float x, float y, Vector3 bottom1, Vector3 bottom2, Vector3 top1, Vector3 top2)
+	{
+		Vector3 bottomRow = Vector3.Lerp(bottom1, bottom2, x);
+		Vector3 topRow = Vector3.Lerp(top1, top2, x);
+
+		Vector3 blendBetweenRows = Vector3.Lerp(bottomRow, topRow, y);
+
+		return blendBetweenRows;
+	}
+}
