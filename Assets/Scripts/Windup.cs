20f4f97782c26c5e076518c73ccc308b1849ad43
Boothand <boothand@hotmail.com>
2017-02-28 17:03:56 +0100

    Attack test.

---
+++ Assets/Scripts/Windup.cs
@@ -0,0 +1,31 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class Windup : MechComponent
+{
+	bool windingUp;
+	bool attacking;
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	void Update()
+	{
+		if (!windingUp && input.attack)
+		{
+			windingUp = true;
+
+			animator.CrossFade("Windup Left", 0.25f, 1);
+		}
+
+		if (windingUp && !input.attack)
+		{
+			windingUp = false;
+			attacking = true;
+
+			animator.CrossFade("Left Slash", 0.25f, 1);
+		}
+	}
+}
