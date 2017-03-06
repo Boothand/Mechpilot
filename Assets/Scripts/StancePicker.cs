94c6508f1d4dea61bb175de381ae7afd6522c499
Boothand <boothand@hotmail.com>
2017-03-06 13:49:03 +0100

    Always go via the mid pose when switching certain stances

--- Assets/Scripts/StancePicker.cs
+++ Assets/Scripts/StancePicker.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class StancePicker : MechComponent
 {
-	[SerializeField] Transform trTransform, tlTransform, brTransform, blTransform, topTransform;
+	[SerializeField] Transform trTransform, tlTransform, brTransform, blTransform, topTransform, bottomMidPoseTransform;
 	public WeaponsOfficer.CombatDir stance { get; private set; }
 	WeaponsOfficer.CombatDir prevStance;
 
@@ -46,17 +46,53 @@ public class StancePicker : MechComponent
 	IEnumerator ChangeStanceRoutine(WeaponsOfficer.CombatDir newStance)
 	{
 		changingStance = true;
+		bool goToMid = false;
 		Transform rIK = arms.getRhandIKTarget;
 		Vector3 fromPos = rIK.position;
 		Quaternion fromRot = rIK.rotation;
 		float timer = 0f;
 
-		while (timer < switchTime)
+		float switchTimeToUse = switchTime;
+
+		if (prevStance != WeaponsOfficer.CombatDir.Top && newStance != WeaponsOfficer.CombatDir.Top)
+		{
+			if (prevStance == WeaponsOfficer.CombatDir.BottomLeft && newStance == WeaponsOfficer.CombatDir.BottomRight ||
+				prevStance == WeaponsOfficer.CombatDir.BottomRight && newStance == WeaponsOfficer.CombatDir.BottomLeft ||
+				prevStance == WeaponsOfficer.CombatDir.TopLeft && newStance == WeaponsOfficer.CombatDir.TopRight ||
+				prevStance == WeaponsOfficer.CombatDir.TopRight && newStance == WeaponsOfficer.CombatDir.TopLeft ||
+				prevStance == WeaponsOfficer.CombatDir.BottomLeft && newStance == WeaponsOfficer.CombatDir.TopRight ||
+				prevStance == WeaponsOfficer.CombatDir.BottomRight && newStance == WeaponsOfficer.CombatDir.TopLeft ||
+				prevStance == WeaponsOfficer.CombatDir.TopLeft && newStance == WeaponsOfficer.CombatDir.BottomRight ||
+				prevStance == WeaponsOfficer.CombatDir.TopRight && newStance == WeaponsOfficer.CombatDir.BottomLeft)
+			{
+				goToMid = true;
+				switchTimeToUse /= 2f;
+			}
+		}
+
+		if (goToMid)
+		{
+			while (timer < switchTimeToUse)
+			{
+				timer += Time.deltaTime;
+
+				rIK.position = Vector3.Lerp(fromPos, bottomMidPoseTransform.position, timer / switchTimeToUse);
+				rIK.rotation = Quaternion.Lerp(fromRot, bottomMidPoseTransform.rotation, timer / switchTimeToUse);
+
+				yield return new WaitForEndOfFrame();
+			}
+		}
+
+		timer = 0f;
+		fromPos = rIK.position;
+		fromRot = rIK.rotation;
+
+		while (timer < switchTimeToUse)
 		{
 			timer += Time.deltaTime;
 
-			rIK.position = Vector3.Lerp(fromPos, targetTransform.position, timer / switchTime);
-			rIK.rotation = Quaternion.Lerp(fromRot, targetTransform.rotation, timer / switchTime);
+			rIK.position = Vector3.Lerp(fromPos, targetTransform.position, timer / switchTimeToUse);
+			rIK.rotation = Quaternion.Lerp(fromRot, targetTransform.rotation, timer / switchTimeToUse);
 
 			yield return new WaitForEndOfFrame();
 		}
