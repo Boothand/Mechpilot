37267fb3338f977e63e28f793cb1c77015cd0957
Boothand <boothand@hotmail.com>
2017-03-20 14:28:37 +0100

    Tween between the positions

--- Assets/Scripts/CameraPositionSwitch.cs
+++ Assets/Scripts/CameraPositionSwitch.cs
@@ -1,9 +1,13 @@
-﻿//using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class CameraPositionSwitch : MechComponent
 {
 	[SerializeField] Transform behind, left, right, firstperson;
+	Transform targetPos;
+	Transform prevTargetPos;
+	[SerializeField] float switchTime = 0.5f;
+	bool switching;
 
 	protected override void OnAwake()
 	{
@@ -12,36 +16,62 @@ public class CameraPositionSwitch : MechComponent
 
 	void Start ()
 	{
-		transform.SetParent(behind);
-		transform.localPosition = Vector3.zero;
-		transform.localRotation = Quaternion.identity;
+		targetPos = behind;
+	}
+
+	IEnumerator SwitchRoutine()
+	{
+		float timer = 0f;
+		Vector3 fromPos = transform.position;
+		Quaternion fromRot = transform.rotation;
+		Vector3 toPos = targetPos.position;
+		Quaternion toRot = targetPos.rotation;
+		prevTargetPos = targetPos;
+
+		while (timer < switchTime)
+		{
+			timer += Time.deltaTime;
+			float smoothTime = Mathf.SmoothStep(0f, 1f, timer / switchTime);
+			transform.position = Vector3.Lerp(fromPos, toPos, smoothTime);
+			transform.rotation = Quaternion.Lerp(fromRot, toRot, smoothTime);
+
+			yield return null;
+		}
+
+		switching = false;
 	}
 
 	void Update()
 	{
 		if (input.camLeft)
 		{
-			transform.SetParent(left);
-			transform.localPosition = Vector3.zero;
-			transform.localRotation = Quaternion.identity;
+			targetPos = left;
 		}
 		else if (input.camRight)
 		{
-			transform.SetParent(right);
-			transform.localPosition = Vector3.zero;
-			transform.localRotation = Quaternion.identity;
+			targetPos = right;
 		}
 		else if (input.camBehind)
 		{
-			transform.SetParent(behind);
-			transform.localPosition = Vector3.zero;
-			transform.localRotation = Quaternion.identity;
+			targetPos = behind;
 		}
 		else if (input.camFP)
 		{
-			transform.SetParent(firstperson);
-			transform.localPosition = Vector3.zero;
-			transform.localRotation = Quaternion.identity;
+			targetPos = firstperson;
+		}
+
+		if (!switching
+			&& prevTargetPos != targetPos)
+		{
+			switching = true;
+			StopAllCoroutines();
+			StartCoroutine(SwitchRoutine());
+		}
+
+		if (!switching)
+		{
+			transform.position = targetPos.position;
+			transform.rotation = targetPos.rotation;
 		}
 	}
 }
