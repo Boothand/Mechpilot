43e0aaf6e9ccf1ede377c652bdbffe3609db226a
Mathias <glmathias92@gmail.com>
2017-03-23 13:22:47 +0100

    Added a new updated version of the level.

---
+++ Assets/Scripts/ZeroGravity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroGravity : MonoBehaviour {
+
+    public GameObject player;
+    private UnityStandardAssets.Characters.FirstPerson.FirstPersonController charCTRL;
+    private bool gravity;
+
+    // Use this for initialization
+    void Start ()
+    {
+        charCTRL = player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        gravity = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(gravity)
+        {
+            case true:
+                charCTRL.m_GravityMultiplier = 2;
+                if(Input.GetKeyDown(KeyCode.F))
+                {
+                    gravity = false;
+                }
+                break;
+            case false:
+                charCTRL.m_GravityMultiplier = 0;
+                if(Input.GetKeyDown(KeyCode.F))
+                {
+                    gravity = true;
+                }
+                break;
+        }
+		
+	}
+}
