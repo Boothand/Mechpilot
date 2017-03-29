fc648faf247311951a786bc96a629fe2701a36a7
Mathias <glmathias92@gmail.com>
2017-03-29 14:59:40 +0200

    Made some modifications to the level and prepped it for play testing and added a death script. Also added a script that activates God Mode for the first person shooter when 'f' is pressed.

---
+++ Assets/Scripts/GodMode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodMode : MonoBehaviour {
+
+    private UnityStandardAssets.Characters.FirstPerson.FirstPersonController charCTRL;
+    private bool grav;
+
+	void Start ()
+    {
+        charCTRL = GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        grav = false;
+	}
+	
+	
+    // Update is called once per frame
+	void Update ()
+    {
+        switch (grav)
+        {
+            case true:
+                charCTRL.m_GravityMultiplier = 0;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    grav = false;
+                }
+                break;
+
+            case false:
+                charCTRL.m_GravityMultiplier = 2;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    grav = true;
+                }
+                break;
+        }
+    }
+}
