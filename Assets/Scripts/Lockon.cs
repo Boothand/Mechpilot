7fafc9c223bbb4848c2bb4f358741dee3eec6b32
Boothand <boothand@hotmail.com>
2017-04-19 09:46:23 +0200

    Automatically change camera preset when locking on and off

--- Assets/Scripts/Lockon.cs
+++ Assets/Scripts/Lockon.cs
@@ -5,6 +5,9 @@ public class Lockon : MechComponent
 {
 	public bool lockedOn { get; private set; }
 
+	public System.Action OnLockOn;
+	public System.Action OnLockOff;
+
 	protected override void OnAwake()
 	{
 		base.OnAwake();
@@ -17,16 +20,25 @@ public class Lockon : MechComponent
 			if (!lockedOn && mech.tempEnemy)
 			{
 				lockedOn = true;
+
+				if (OnLockOn != null)
+					OnLockOn();
 			}
 			else if (lockedOn)
 			{
 				lockedOn = false;
+
+				if (OnLockOff != null)
+					OnLockOff();
 			}
 		}
 
-		if (mech.tempEnemy == null)
+		if (lockedOn && mech.tempEnemy == null)
 		{
 			lockedOn = false;
+
+			if (OnLockOff != null)
+				OnLockOff();
 		}
 	}
 }
