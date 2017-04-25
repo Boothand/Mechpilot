491ffae637e3d2f546032cbb1e7c2ec8d2951bf7
Boothand <boothand@hotmail.com>
2017-04-25 16:56:44 +0200

    Don't lock the angles when not colliding anymore, causes glitchiness. Was there to have improved accuracy on animation, but probably not necessary.

--- Assets/Scripts/WeaponsOfficer.cs
+++ Assets/Scripts/WeaponsOfficer.cs
@@ -54,14 +54,14 @@ public class WeaponsOfficer : MechComponent
 	void Start()
 	{
 		//IgnoreHierarchyRecursive(transform.root, weapon.GetComponent<Collider>());
-		StartCoroutine(LockWeaponMotionRoutine());	//Hack: Lock it one frame after start, to override puppetmaster?
+		//StartCoroutine(LockWeaponMotionRoutine());	//Hack: Lock it one frame after start, to override puppetmaster?
 	}
 
-	IEnumerator LockWeaponMotionRoutine()
-	{
-		yield return null;
-		weapon.LockSwordAngularMotion(true);
-	}
+	//IEnumerator LockWeaponMotionRoutine()
+	//{
+	//	yield return null;
+	//	weapon.LockSwordAngularMotion(true);
+	//}
 
 	IEnumerator TweenLayerWeightRoutine(float weight, int layer, float time)
 	{
