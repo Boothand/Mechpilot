fbbde98305777d32092e24510bb5044cc3c56643
Boothand <boothand@hotmail.com>
2017-05-02 12:44:35 +0200

    UI panel above head faces opponent's camera. Fixed particle effect radius.

---
+++ Assets/Scripts/FaceOpponentCamera.cs
@@ -0,0 +1,23 @@
+﻿//using System.Collections;
+using UnityEngine;
+
+public class FaceOpponentCamera : MechComponent
+{
+
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		//Face the opponent's camera
+		if (mech.tempEnemy)
+		{
+			transform.forward = mech.tempEnemy.pilot.cameraFollow.transform.position - transform.position;
+		}
+	}
+}
--- Assets/Scripts/UI/LockonIndicator.cs
+++ Assets/Scripts/UI/LockonIndicator.cs
@@ -39,12 +39,5 @@ public class LockonIndicator : MechComponent
 	{
 		//Stay above head
 		indicatorImg.transform.position = hierarchy.head.position + Vector3.up * verticalOffset;
-
-		//Face the opponent's camera
-		if (mech.tempEnemy &&
-			indicatorImg.enabled)
-		{
-			transform.forward = mech.tempEnemy.pilot.cameraFollow.transform.position - transform.position;
-		}
 	}
 }
