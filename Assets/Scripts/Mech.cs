acfef3309c64b151af64eda0e86fffd53576d62c
Boothand <boothand@hotmail.com>
2017-05-09 16:19:12 +0200

    Added some convenience code so you don't have to hook up so much in the inspector.

--- Assets/Scripts/Mech.cs
+++ Assets/Scripts/Mech.cs
@@ -8,9 +8,27 @@ public class Mech : MonoBehaviour
 	public WeaponsOfficer weaponsOfficer { get; private set; }
 	public Mech tempEnemy;
 
+	public enum PlayerIndex { One, Two }
+	public PlayerIndex playerIndex;
+
 	void Awake()
 	{
 		pilot = transform.root.GetComponentInChildren<Pilot>();
 		weaponsOfficer = transform.root.GetComponentInChildren<WeaponsOfficer>();
+
+		Mech[] mechs = FindObjectsOfType<Mech>();
+
+		for (int i = 0; i < mechs.Length; ++i)
+		{
+			if (mechs[i].playerIndex != playerIndex)
+			{
+				tempEnemy = mechs[i];
+				break;
+			}
+			else
+			{
+				Debug.LogWarning("Two mechs exist with the same playerIndex. Change one of them.");
+			}
+		}
 	}
 }
